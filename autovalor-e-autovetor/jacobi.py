89a8e0416b5b287987d829aae390dc19c918df84
Marcos Augusto <mjaojr@gmail.com>
2016-06-20 19:56:17 -0300

    (Eigenvalue and eigenvector): Finished Jacobi's method

--- autovalor-e-autovetor/jacobi.py
+++ autovalor-e-autovetor/jacobi.py
@@ -5,8 +5,22 @@ from numpy import *
 # Matricula: 370021
 
 def iniciarMatriz():
-    arquivo = open('matriz.db', 'r')
+    arquivo = open('matriz2.db', 'r')
     return matrix([ map(float,linha.split('|')[0].split(',')) for linha in arquivo ])
+
+def definirTolerancia():
+    arquivo = open('matriz2.db', 'r')
+    return float(arquivo.readline().split('|')[1])
+
+def iniciarSistema():
+    return iniciarMatriz(), definirTolerancia()
+
+def toleravel(old, new, tol):
+    return (absolute(old.round(5)-new.round(5)) <= tol*absolute(new)).all()
+
+def arredondar(A, D, J):
+    fator = 5
+    return A.round(fator).tolist(), D.round(fator).tolist(), J.round(fator).tolist()
 
 def imprimirNaTela(A, D, J):
     print('\n----- Resultado -----')
@@ -54,20 +68,24 @@ def matrizRotacao(i, j, tempA):
 
     return r
 
-def algoritmoJacobi(A):
+def algoritmoJacobi(A, tol):
+    nValido = 1
 
     n = len(A)
     tempA = A
     J = identity(n)
-    for j in range(0,n):
-          for i in range(j+1,n):
+    while nValido:
+        for j in range(0,n-1):
+            for i in range(j+1,n):
                 tempJ = matrizRotacao(i,j,tempA)
-                print tempJ
+                oldA = tempA
                 tempA = tempJ.transpose()*tempA*tempJ
-                J = tempJ*J
-                print tempA
+                J = J.dot(tempJ)
 
-    return D;
+        if(toleravel(oldA, tempA, tol)):
+            nValido = 0
+
+    return tempA, J;
 
 def main():
     print "\n################## SEJA BEM-VINDO! ##################"
@@ -75,10 +93,11 @@ def main():
 
 
     iniciar = raw_input("Tecle [ENTER] para iniciar\n")
-    A = iniciarMatriz()
+    A, tol = iniciarSistema()
 
-    D, J = algoritmoJacobi(A)
+    D, J = algoritmoJacobi(A, tol)
+    A, D, J = arredondar(A, D, J)
 
-    #imprimirNaTela(A, T, H)
-    #imprimirEmArquivo(A, T, H)
+    imprimirNaTela(A,D,J)
+    imprimirEmArquivo(A, D, J)
 main()
